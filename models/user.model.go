c8b4c2016114bf88282a4bebf98d1af0babacfb6
Raheem Kawojue <kawojue08@gmail.com>
2023-12-31 15:54:24 +0100

    [add] user model

---
+++ models/user.model.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Users struct {
+	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	Email     string    `gorm:"uniqueIndex"`
+	Username  string    `gorm:"uniqueIndex"`
+	Password  string
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `gorm:"default:autoUpdateTime"`
+}
