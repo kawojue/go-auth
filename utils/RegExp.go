bf135cb976c521a633eb8ae1268d51ea06592145
Raheem Kawojue <kawojue08@gmail.com>
2024-01-01 03:51:27 +0100

    [add] regexp

--- utils/RegExp.go
+++ utils/RegExp.go
@@ -1,1 +1,7 @@
 package utils
+
+import "regexp"
+
+var EmailRegex = regexp.MustCompile(`^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com|icloud\.com|fastmail\.com)$`)
+
+var UsernameRegex = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9_-]{2,15}$`)
