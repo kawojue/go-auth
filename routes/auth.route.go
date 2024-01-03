b1e4676266884754e2c2e5ea6110b1cdcd95e411
Raheem Kawojue <kawojue08@gmail.com>
2024-01-03 01:00:13 +0100

    [edit] auth routes

---
+++ routes/auth.route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kawojue/go-auth/controllers"
+)
+
+func AuthRoutes(router *gin.Engine) {
+	auth := router.Group("/auth")
+	{
+		auth.POST("/signup", controllers.SignUp)
+		auth.POST("/login", controllers.Login)
+	}
+}
