4d758adc7f058f94b42de679ebf4364beb5a3c0b
Raheem Kawojue <kawojue08@gmail.com>
2024-01-07 00:14:52 +0100

    on it..

--- routes/auth.route.go
+++ routes/auth.route.go
@@ -2,27 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	rate "github.com/kawojue/gin-ratelimiter"
 	"github.com/kawojue/go-auth/controllers"
 )
 
 func AuthRoutes(router *gin.Engine) {
-	limiterConfig := rate.LimiterConfig{
-		MaxAttempts: 5,
-		TimerArray:  []int{15, 14, 19},
-		Message:     "Too many requests",
-	}
-
-	limiter := rate.CreateLimiter(&limiterConfig)
-
 	auth := router.Group("/auth")
 	{
 		auth.POST("/signup", controllers.SignUp)
-		auth.POST(
-			"/login",
-			rate.RateLimiter(limiter, &limiterConfig),
-			controllers.Login,
-		)
+		auth.POST("/login", controllers.Login)
 		auth.POST("/logout", controllers.Logout)
 		auth.POST("/refresh", controllers.RefreshToken)
 	}
