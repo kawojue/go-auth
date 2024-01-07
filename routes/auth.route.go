abfaf5d0e54bc61540d4cb62f22f33cc5c95b730
Raheem Kawojue <kawojue08@gmail.com>
2024-01-08 00:28:30 +0100

    [add] ratelimiter middleware to login route

--- routes/auth.route.go
+++ routes/auth.route.go
@@ -2,16 +2,29 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	rate "github.com/kawojue/gin-ratelimiter"
 	"github.com/kawojue/go-auth/controllers"
 )
 
 var auth *gin.RouterGroup
 
 func AuthRoutes(router *gin.RouterGroup) {
+	limiterConfig := rate.LimiterConfig{
+		MaxAttempts: 5,
+		TimerArray:  []int{10, 8, 20},
+		Message:     "Too many request.",
+	}
+
+	limiter := rate.CreateLimiter(&limiterConfig)
+
 	auth = router.Group("/auth")
 	{
 		auth.POST("/signup", controllers.SignUp)
-		auth.POST("/login", controllers.Login)
+		auth.POST(
+			"/login",
+			rate.RateLimiter(limiter, &limiterConfig),
+			controllers.Login,
+		)
 		auth.POST("/logout", controllers.Logout)
 		auth.POST("/refresh", controllers.RefreshToken)
 	}
