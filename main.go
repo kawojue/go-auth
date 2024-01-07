3fac1bb78c958b6892c66755eac7322db03d9f20
Raheem Kawojue <kawojue08@gmail.com>
2024-01-08 00:28:41 +0100

    ..

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	gologger "github.com/kawojue/gin-gologger"
-	rate "github.com/kawojue/gin-ratelimiter"
 	"github.com/kawojue/go-auth/configs"
 	"github.com/kawojue/go-auth/routes"
 	initenv "github.com/kawojue/go-initenv"
@@ -33,15 +32,7 @@ func main() {
 
 	router.Use(gologger.Logger(gin.ReleaseMode))
 
-	limiterConfig := rate.LimiterConfig{
-		MaxAttempts: 1,
-		TimerArray:  []int{7},
-		Message:     "Too many request.",
-	}
-
-	limiter := rate.CreateLimiter(&limiterConfig)
-
-	router.GET("/", rate.RateLimiter(limiter, &limiterConfig), func(ctx *gin.Context) {
+	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Go Auth",
 		})
