18e844d2d8a33ed8ab5ed1bbadaa188d15f74e4d
Raheem Kawojue <kawojue08@gmail.com>
2024-01-09 11:02:10 +0100

    [fixed] forgot pswd controller

--- controllers/password.controller.go
+++ controllers/password.controller.go
@@ -15,9 +15,10 @@ import (
 
 func ForgotPassword(ctx *gin.Context) {
 	var (
-		err  error
-		user *models.Users
-		body structs.ForgotPassword
+		err          error
+		user         models.Users
+		existingTotp models.TOTP
+		body         structs.ForgotPassword
 	)
 
 	if err = ctx.ShouldBindJSON(&body); err != nil {
@@ -31,20 +32,29 @@ func ForgotPassword(ctx *gin.Context) {
 		return
 	}
 
-	totp := utils.GenOTP(5)
-
-	totpRecord := models.TOTP{
-		Otp:       totp.Otp,
-		OtpExpiry: totp.Otp_Expiry,
-		UserID:    user.ID,
-	}
-
-	if err = configs.DB.Create(&totpRecord).Error; err != nil {
-		helpers.SendError(ctx, http.StatusInternalServerError, "Failed to save OTP in the database.")
-		return
-	}
+	totp := utils.GenOTP(6)
 
 	helpers.SendMail([]string{user.Email}, fmt.Sprintf("Subject: One-time passcode\r\n\r\nOTP: %s", totp.Otp))
+
+	if err = configs.DB.Where("user_id", user.ID.String()).First(&existingTotp).Error; err != nil {
+		totpRecord := models.TOTP{
+			Otp:       totp.Otp,
+			OtpExpiry: totp.Otp_Expiry,
+			UserID:    user.ID,
+		}
+		if err = configs.DB.Create(&totpRecord).Error; err != nil {
+			helpers.SendError(ctx, http.StatusInternalServerError, "Failed to save OTP in the database.")
+			return
+		}
+	} else {
+		if err = configs.DB.Model(&models.TOTP{}).Where("user_id", user.ID.String()).Updates(&models.TOTP{
+			Otp:       totp.Otp,
+			OtpExpiry: totp.Otp_Expiry,
+		}).Error; err != nil {
+			helpers.SendError(ctx, http.StatusInternalServerError, "Failed to save OTP in the database.")
+			return
+		}
+	}
 
 	helpers.SendSuccess(ctx, http.StatusOK, "An OTP has been sent to your email.", nil)
 }
