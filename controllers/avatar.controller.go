7bf3a9bf0ee8c8fdb8e35a0c9e35fb8d3a215fb1
Raheem Kawojue <kawojue08@gmail.com>
2024-01-10 02:06:46 +0100

    .. on it

---
+++ controllers/avatar.controller.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kawojue/go-auth/configs"
+	"github.com/kawojue/go-auth/helpers"
+	"github.com/kawojue/go-auth/models"
+)
+
+func UploadAvatar(ctx *gin.Context) {
+	var (
+		err  error
+		user models.Users
+	)
+
+	user_id, exists := ctx.Get("user_id")
+	if !exists {
+		helpers.SendError(ctx, http.StatusUnauthorized, "Something went wrong.")
+		return
+	}
+
+	if err = configs.DB.Where("id = ?", user_id).First(&user).Error; err != nil {
+		helpers.SendError(ctx, http.StatusNotFound, "Account not found.")
+		return
+	}
+
+}
+
+func DeleteAvatar(ctx *gin.Context) {
+
+}
