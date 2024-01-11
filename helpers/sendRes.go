1eb8db857529330b03c9fb627460739acff44297
Raheem Kawojue <kawojue08@gmail.com>
2024-01-11 23:36:58 +0100

    [add] err msg

--- helpers/sendRes.go
+++ helpers/sendRes.go
@@ -1,6 +1,10 @@
 package helpers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SendSuccess(ctx *gin.Context, statusCode int, msg string, data interface{}) {
 	ctx.JSON(statusCode, gin.H{
@@ -15,4 +19,31 @@ func SendError(ctx *gin.Context, statusCode int, msg string) {
 		"success": false,
 		"error":   msg,
 	})
+}
+
+var SOMETHING_WENT_WRONG = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusInternalServerError, "Something went wrong")
+}
+
+var ACCOUNT_NOT_FOUND = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusNotFound, "Account not found")
+}
+
+var INVALID_JSON = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusInternalServerError, "Invalid JSON")
+}
+
+var FAILED_TO_SAVE_OTP = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusInternalServerError, "Failed to save OTP in the database")
+}
+
+var ALL_FIELDS_REQUIRED = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusBadRequest, "All fields are required")
+}
+
+var FORBIDDEN_ACESS_DENIED = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusForbidden, "Access denied")
+}
+var UNAUTHORIZED_ACESS_DENIED = func(ctx *gin.Context) {
+	SendError(ctx, http.StatusUnauthorized, "Access denied")
 }
