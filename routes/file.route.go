87c8f13a0cb400254049282a9eb41d12a6bd0659
Raheem Kawojue <kawojue08@gmail.com>
2024-01-14 00:28:13 +0100

    .

--- routes/file.route.go
+++ routes/file.route.go
@@ -1,12 +1,22 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kawojue/go-auth/controllers"
+	"github.com/kawojue/go-auth/middlewares"
+)
 
 var file *gin.RouterGroup
 
 func FileRoutes(router *gin.RouterGroup) {
 	file = router.Group("/file")
 	{
+		file.POST(
+			"/avatar",
+			middlewares.VerifyAuth(),
+			middlewares.LimitRequestBodySize(5<<20),
+			controllers.UploadAvatar,
+		)
 
 	}
 }
