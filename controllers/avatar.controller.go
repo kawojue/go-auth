d95b928c34e7b6cf99cb3511f9fc816aab4c3452
Raheem Kawojue <kawojue08@gmail.com>
2024-01-13 21:57:05 +0100

    [edit] file handler

--- controllers/avatar.controller.go
+++ controllers/avatar.controller.go
@@ -7,27 +7,42 @@ import (
 	"github.com/kawojue/go-auth/configs"
 	"github.com/kawojue/go-auth/helpers"
 	"github.com/kawojue/go-auth/models"
+	"github.com/kawojue/go-auth/utils"
 )
 
 func UploadAvatar(ctx *gin.Context) {
 	var (
 		err  error
+		existingAvatar models.Avatars
 		user models.Users
 	)
 
 	user_id, exists := ctx.Get("user_id")
 	if !exists {
-		helpers.SendError(ctx, http.StatusUnauthorized, "Something went wrong.")
+		helpers.SOMETHING_WENT_WRONG(ctx)
 		return
 	}
 
 	if err = configs.DB.Where("id = ?", user_id).First(&user).Error; err != nil {
-		helpers.SendError(ctx, http.StatusNotFound, "Account not found.")
+		helpers.ACCOUNT_NOT_FOUND(ctx)
 		return
 	}
 
-	// file, handler, err := ctx.Request.FormFile("avatar")
+	avatar, header, err := ctx.Request.FormFile("avatar")
+	if err != nil {
+		return
+	}
+	defer avatar.Close()
 
+	file, err := utils.HandleFile(ctx, 5<<20, header, avatar, "jpg", "png")
+	if err != nil {
+		return
+	}
+
+	helpers.UploadS3(ctx, file.FileName, file.FileBytes)
+	avatar_url := helpers.GetS3(file.FileName)
+
+	if err = configs.DB.
 }
 
 func DeleteAvatar(ctx *gin.Context) {
