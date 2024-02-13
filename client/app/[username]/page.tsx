94e1ec3d52decc214dc7647081f40172f3231cc8
Raheem Kawojue <kawojue08@gmail.com>
2024-02-13 17:25:55 +0100

    [fetch] username

--- client/app/[username]/page.tsx
+++ client/app/[username]/page.tsx
@@ -1,8 +1,33 @@
 "use client"
+import { axios } from '../api/axios'
+import { Spin } from '@/components/Loading'
+import throwError from '@/utils/throwError'
+import { useQuery } from '@tanstack/react-query'
+import { AxiosError, AxiosResponse } from 'axios'
 
 const page = ({ params: { username } }: Params) => {
+    const { data, isLoading } = useQuery({
+        queryKey: ['user'],
+        queryFn: async () => {
+            return await axios.get(`/${username}`)
+                .then(({ data }: AxiosResponse) => {
+                    return data
+                }).catch((err: AxiosError) => {
+                    throwError(err)
+                })
+        }
+    })
+
+    if (isLoading) {
+        return <Spin />
+    }
+
+    console.log(data)
+
     return (
-        <>{username}</>
+        <>
+            {username}
+        </>
     )
 }
 
