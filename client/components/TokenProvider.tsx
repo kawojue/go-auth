a5fb8fd64ebe6d23feb41b52c00b7e8df2d846f9
Raheem Kawojue <kawojue08@gmail.com>
2024-02-13 14:26:47 +0100

    [add] token provider

--- client/components/TokenProvider.tsx
+++ client/components/TokenProvider.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { useEffect, FC, ReactNode } from 'react'
 import getCookie from '@/utils/getCookie'
 import { userStore } from '@/store/authStore'
+import { useEffect, FC, ReactNode } from 'react'
 
 export const TokenProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const { user, setUser } = userStore()
